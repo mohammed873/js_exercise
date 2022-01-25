e823db5b454f815156fc547880c118f97a3d8439
mohammed elhachimi <mohammed.elhachimi@halsson.com>
2022-01-25 13:42:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// let str = "Artisan is the command-line interface included with Laravel. It provides a number of helpful commands that can assist you while you build your application. To view a list of all available Artisan commands, you may use the list command:";
+
+// console.log(str);
+
+// // let mostRepatedWords = [];
+
+// let array = str.split(" ");
+// console.log(array);
+
+// array.forEach(item => console.log(item))
+
+// const checkDoubleWords = (words) => {
+//     let repeatedWords = [];
+//     words.filter(word => word == word ? repeatedWords.push(word) : word)
+// } 
+
+
+// 
+
+// let str = "Artisan is the command-line interface included with Laravel. It provides a number of helpful commands that can assist you while you build your application. To view a list of all available Artisan commands, you may use the list command:";
+
+// console.log(str);
+
+// let array = str.split(" ");
+// console.log(array);
+
+// let newArray = [];
+// array.map(item => {
+//     if(item.length > 4){
+//         newArray.push(item)
+//     }
+//    return newArray ;
+// })
+
+// console.log(newArray.length);
+
+// let numbers= [];
+// for (let i = 0; i < 101; i++) {
+//     // console.log(i);
+//     numbers.push(i);
+// }
+
+
+
+
+
